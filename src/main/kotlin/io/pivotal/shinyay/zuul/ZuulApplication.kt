eda38c50f3c06969d24261d111ce6ce4a91e5f4a
shinyay <shinya.com@gmail.com>
2019-05-03 17:33:33 +0900

    Add Enable EurekaClient

--- src/main/kotlin/io/pivotal/shinyay/zuul/ZuulApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/zuul/ZuulApplication.kt
@@ -2,10 +2,12 @@ package io.pivotal.shinyay.zuul
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 class ZuulApplication
 
 fun main(args: Array<String>) {
